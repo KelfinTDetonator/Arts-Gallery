79caf43143fcce43c76a747713610c401e18a0fb
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-02 02:45:52 +0700

    fix: profile schema

---
+++ src/controllers/media.controller.js
@@ -0,0 +1,9 @@
+const { users, profiles, collections } = require('../../models/prismaClient')
+const response = require('../../utils/response.utils')
+const { CustomError } = require('../../utils/customError.utils')
+
+module.exports = {
+    imageKitUpload: async (req, res) => {
+        
+    }
+}
