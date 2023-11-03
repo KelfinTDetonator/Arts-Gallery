11bfc7b1b13d6449ffe90f72ca824f666dfc8ba3
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-03 15:53:54 +0700

    add: upload router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const router = require('./router')
 const users = require('./users.routes');
+const collections = require('./media.routes')
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
 router.use(users);
+router.use(collections);
 
 module.exports = router
