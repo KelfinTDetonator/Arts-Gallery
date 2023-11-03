b6398c2697ffbaa81e88296ec6ad68e0881caabd
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-03 15:54:07 +0700

    add: media upload router

---
+++ src/routes/media.routes.js
@@ -0,0 +1,16 @@
+const router = require('./router');
+const controller = require('../controllers/collection.controller')
+const storage = require('../middlewares/multer');
+
+router.get('/upload/images', (req, res)=> {
+    return res.render('form')
+})
+router.post('/upload/images', storage.upload.single('photo'), controller.uploadCollection);
+
+//upload to imagekit.io
+
+const multer = require('multer')();
+const imagekitController = require('../controllers/imageKit.controller')
+router.post('/imagekit', storage.upload.single('photo') , imagekitController.imageKitUpload)
+
+module.exports = router
