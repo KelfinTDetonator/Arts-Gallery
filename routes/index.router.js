84ac612c81f023395a6627c9c71ea6c18842871c
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-04 18:35:05 +0700

    delete

--- routes/index.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('./router')
-
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-module.exports = router
--- src/controllers/media.controller.js
+++
@@ -1,9 +0,0 @@
-const { users, profiles, collections } = require('../../models/prismaClient')
-const response = require('../../utils/response.utils')
-const { CustomError } = require('../../utils/customError.utils')
-
-module.exports = {
-    imageKitUpload: async (req, res) => {
-        
-    }
-}
