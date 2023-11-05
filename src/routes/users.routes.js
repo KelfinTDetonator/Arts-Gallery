c220eebd5bb641fc6034eb7300b7fb2d7c85d4d5
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-06 00:16:54 +0700

    add: new endpoint

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,6 @@
 const router = require('./router')
 const controller = require('../controllers/users.controller')
 
-
 router.post('/register', controller.createUser)
-
+router.get('/user/:id', controller.getUserById)
 module.exports = router
